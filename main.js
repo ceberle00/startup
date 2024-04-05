a1855d98753defba079b845c981f0648fdb04b37
ceberle00 <112594166+ceberle00@users.noreply.github.com>
2024-04-04 18:41:07 -0600

    renamed js file for services

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 //might just do everything in here lol
 //next issue, need to pass in playlist info to be able to look at songs
+//SERVICE PART?
+const http = require('http');
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
+  res.end();
+});
 
+server.listen(8080, () => {
+  console.log(`Web service listening on port 8080`);
+});
 //MAINPAGE AND ADDPLAY HTML FILES
 var playlists = new Map();
 
